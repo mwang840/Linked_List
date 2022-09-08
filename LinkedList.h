8a8ec80d90e886f19aee61f9a96bba825f304a56
mwang840 <maxwang@udel.edu>
2022-09-08 13:27:28 -0400

    structured functions below the struct

--- LinkedList.h
+++ LinkedList.h
@@ -1,7 +1,5 @@
 #include <stdio.h>
-
-char *getStudent(char *first, char *last, long id, char *year, int expected);
-
+#include <stdlib.h>
 
 typedef struct student{
     char *firstName;
@@ -12,3 +10,12 @@ typedef struct student{
     struct student *next;
 
 } student_t;
+
+char *getStudent(char *first, char *last, long id, char *year, int expected);
+void printListForward(student_t *head);
+void printListBackwards(student_t *tail);
+student_t *pop();
+void push(student_t *head, long id);
+int totalStudents(student_t *head);
+int removeLast(student_t *head);
+int removeById();
