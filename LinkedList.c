6a3fdd7eaef92b74e549888e463fa890d395a33a
mwang840 <maxwang@udel.edu>
2022-09-10 21:29:58 -0400

    added printf statements

--- LinkedList.c
+++ LinkedList.c
@@ -28,6 +28,28 @@ void printListBackwards(student_t *tail){
     printf("Printing out the list of students in reverse order\n");
     while(current != NULL){
         printf("The students name is %s %s, with an id of %ld, in the class of %s and will be expected to graduate in spring of %d", current->firstName, current->lastName, current->id, current->expected);
+        printf("Student's id: ");
+        printf("\n");
+        printf("\n");
         current = current->prev;
+    }
+}
+
+student_t *pop(){
+    student_t *end = (student_t*)malloc(sizeof(student_t));
+    student_t *temp = end;
+    free(temp);
+    return NULL;
+}
+
+
+void push(student_t *head, long id){
+    student_t *newStudent = (student_t*)malloc(sizeof(student_t));
+    newStudent->id = id;
+    newStudent->prev = NULL;
+    newStudent->next = head;
+
+    if(head != NULL){
+        head->prev = newStudent;
     }
 }
