2ef02dc57e66191fc5334cfdacea8b5d9d58888c
mwang840 <maxwang@udel.edu>
2022-09-13 19:16:04 -0400

    chunk of main file

--- LinkedListMain.c
+++ LinkedListMain.c
@@ -1,8 +1,26 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "LinkedList.h"
-#include <string.h>
 
 int main(){
+    char *studentName = (char*)malloc(28 * sizeof(char));
+    student_t *student = (student_t*)malloc(sizeof(student_t));
+    long id = 0;
+    int selection = 0;
+    printf("What is the students name\n");
+    scanf("%s", studentName);
+    printf("Enter a number from 1-5 for %s\n", studentName);
+    printf("1. Add a student to the list\n");
+    printf("2. Add a student to the list\n");
+    printf("3. Add a student to the list\n");
+    printf("4. Add a student to the list\n");
+    printf("5. Add a student to the list\n");
+    scanf(selection);
+    switch(selection){
+        case 1:
+        
+        break;
+    }
+
     return 0;
 }
