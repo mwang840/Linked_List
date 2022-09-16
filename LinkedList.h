d699c3121099bc796f51203161d42841501f55af
mwang840 <maxwang@udel.edu>
2022-09-16 04:39:22 -0400

    feat header file

--- LinkedList.h
+++ LinkedList.h
@@ -6,10 +6,11 @@ typedef struct student{
     char *year;
     struct student *prev;
     struct student *next;
-    struct student *head;
-    struct student *tail;
 
 } student_t;
+
+student_t *head;
+student_t *tail;
 
 char getStudent(student_t *student);
 void printListForward(student_t *head);
