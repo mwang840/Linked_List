1a51953302a2dd109b076cc4c4b2d9e8761c8f34
mwang840 <maxwang@udel.edu>
2022-09-18 15:05:46 -0400

    added params

--- LinkedList.h
+++ LinkedList.h
@@ -8,11 +8,12 @@ typedef struct student{
     struct student *next;
 
 } student_t;
-void printListForward(student_t *head);
-void printListBackwards(student_t *tail);
+void printListForward();
+void printListBackwards();
 long pop(student_t *node);
 void push(student_t *head, long id);
 int totalStudents(student_t *head);
-void delete(student_t *head, char *last);
+void delete(char *last);
 void addAtEnd(student_t *student, char *first, char *last, char *year);
 void clear();
+void getData(student_t *student, char *oneName, char *serName, char *year);
