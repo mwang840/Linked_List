72c12582f737e8188d73efcf28c0e5e6dae256ab
mwang840 <maxwang@udel.edu>
2022-09-18 15:05:04 -0400

    feat added helper function

--- LinkedListMain.c
+++ LinkedListMain.c
@@ -2,72 +2,57 @@
 #include <stdio.h>
 #include "LinkedList.c"
 
-
+void runProgram();
 
 int main(){
+    runProgram();
+    return 0;
+}
+
+void runProgram(){
     int input = 0;
-    char *oneName, *serName;
-    serName = (char*)(malloc(28 *sizeof(char)));
-    oneName = (char*)(malloc(28 *sizeof(char)));
-    char *year = (char*)(malloc(28 *sizeof(char)));
-    student_t *student = (student_t *)(malloc(sizeof(student_t)));
-    long id = 0;
-    int expeceted = 0;
-    printf("Enter a first name\n");
-    scanf("%s", oneName);
-    printf("Enter a last name\n");
-    scanf("%s", serName);
-    printf("Enter your current year\n");
-    scanf("%s", year);
-    printf("Enter a student id\n");
-    scanf("%ld", &id);
-    scanf("%*[^\n]");
-    scanf("%*c");
-    printf("Enter graduation date\n");
-    scanf("%d", &expeceted);
-    scanf("%*[^\n]");
-    scanf("%*c");
-    printf("1. Add a student to the list\n");
-    printf("2. Deletes a student to the list\n");
-    printf("3. Prints the list from the begining to the end:\n");
-    printf("4. Prints the list from the end to the begining:\n");
-    printf("5. Exit the program\n");
-    scanf("%d", input);
-    student->firstName = oneName;
-    student->lastName = serName;
-    student->year = year;
-    //student->id = id;
-    student->expected = expeceted;
-    
+    //U need to allocate new memory for each try
+    while(1) {
+        char *oneName = (malloc(28 *sizeof(char)));
+        char *serName = (malloc(28 *sizeof(char)));
+        char *year = (malloc(28 *sizeof(char)));
+        student_t *student = (student_t *)(malloc(sizeof(student_t)));
+        printf("1. Add a student to the list\n");
+        printf("2. Deletes a student to the list\n");
+        printf("3. Prints the list from the begining to the end:\n");
+        printf("4. Prints the list from the end to the begining:\n");
+        printf("5. Exit the program\n");
+        scanf("%d", &input);
     switch(input){
         case 1:
-        addAtEnd(student, oneName, serName, year);
-        break;
+            getData(student, oneName, serName, year);
+            break;
         case 2:
             if(head == NULL){
-                printf("Empty list");
+                printf("Empty list\n");
             }
             else{
-                delete(student, serName);
+                char *a;
+                printf("Type the last name you want gone\n");
+                scanf("%s", a);
+                delete(a);
             }
-        break;
+            break;
         case 3:
-            printListForward(student);
+            printListForward();
             break;
         case 4:
-            printListBackwards(student);
+            printListBackwards();
             break;
         case 5:
             clear();
-            break;
+            free(oneName);
+            free(serName);
+            free(student);
+            return;
         default:
-        printf("You entered the %d out of the range 1-5, please try again\n", input);    
-        break;        
+            printf("You entered the %d out of the range 1-5, please try again\n", input);    
+            break;        
+        }
     }
-    free(student->firstName);
-    free(oneName);
-    free(student->lastName);
-    free(serName);
-    free(student);
-    return 0;
 }
